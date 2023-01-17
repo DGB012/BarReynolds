3e8d7ee0782008cd75d488b5d2c1f64844af040a
adri-1010 <adriquise@gmail.com>
2023-01-17 11:04:09 +0100

    todo sobre categoria

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,5 @@ use App\Http\Controllers\ProductoController;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::resource('categorias', CategoriaController::class);
 Route::resource('productos', ProductoController::class);
