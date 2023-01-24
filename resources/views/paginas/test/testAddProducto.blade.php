a512e64793e0a114248790db9f18bf02e3959c33
DGB012 <dagonbu02@gmail.com>
2023-01-24 14:15:20 +0100

    Mostrando todos los productos al crear cuenta, pero falta ocultar los productos según la categoría.

--- resources/views/paginas/test/testAddProducto.blade.php
+++ resources/views/paginas/test/testAddProducto.blade.php
@@ -5,16 +5,6 @@
 
     <br><br>
     <p>{{$cuenta->id}}</p>
-    <button><a href='{{ route('lineaCuenta.crearLineaNueva',['cuenta_id'=>$cuenta->id,'producto_id' => 1]) }}'>Add Coca
-            Cola</a></button>
-    <button><a href='{{ route('lineaCuenta.crearLineaNueva',['cuenta_id'=>$cuenta->id,'producto_id' => 2]) }}'>Add Coca
-            Cola Light</a>
-        </button>
-    <button><a href='{{ route('lineaCuenta.crearLineaNueva',['cuenta_id'=>$cuenta->id,'producto_id' => 3]) }}'>Add Coca
-            Cola Zero</a>
-    </button>
-
-    <button><a href="{{route('cuentas.terminarCuenta',$cuenta->id)}}">Terminar cuenta</a></button>
     @foreach ($categorias as $categoria)
         <button
             onclick="cambiarVisibilidad('{{$categoria->nombre}}', '{{count($categorias)}}')">{{$categoria->nombre}}</button>
