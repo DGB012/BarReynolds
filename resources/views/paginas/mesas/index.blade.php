fdd027dc621110f1696a413061c3019528179de5
SergioSanzCuadros <sergiosanzcuadros@gmail.com>
2023-01-25 09:24:20 +0100

    Cambio enlace index.mesas

--- resources/views/paginas/mesas/index.blade.php
+++ resources/views/paginas/mesas/index.blade.php
@@ -14,23 +14,23 @@
         @foreach($mesas as $mesa)
             <tr>
                 <td>
-                    <a href='{{ route ('productos.index', $mesa) }}'><img src="/mesa.png" width="80px"
-                                                                          height="10%">{{ $mesa-> id }} </a>
+                    <a href='{{ route ('cuentas.crearModifCuenta', $mesa -> id) }}'>
+                        <img src="/mesa.png" width="80px" height="10%">{{ $mesa-> id }}
+                    </a>
                 </td>
                 <td>
                     <a href='{{ route ('cuentas.crearModifCuenta', $mesa -> id) }}'>
-                        <img src='{{($mesa->estado == "Vacia") ? "/verde.png" : "/rojo.png"}}' class="estadoMesa"></a>
+                        <img src='{{($mesa->estado == "Vacia") ? "/verde.png" : "/rojo.png"}}' class="estadoMesa">
+                    </a>
                 </td>
                 <td>
                     <form action='{{ route('mesas.destroy', $mesa) }}' method="post">
                         @method('delete')
-
                         <button type='submit'><img src="basura.png" width="50px"></button>
                     </form>
                 </td>
             </tr>
         @endforeach
-
 
     </table>
     <br><br>
