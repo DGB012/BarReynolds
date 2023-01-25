63af91be484c8db3390cce559cb3495aed51a593
SergioSanzCuadros <sergiosanzcuadros@gmail.com>
2023-01-25 08:59:11 +0100

    Menu para interactuar con los distintos blade (Terminado).

--- resources/views/components/zz/menu.blade.php
+++ resources/views/components/zz/menu.blade.php
@@ -1,7 +1,7 @@
 <nav>
-    <a href='{{ route('productos.index') }}'>Productos</a>
-    <a href='{{ route('categorias.index') }}'>Categor&iacute;as</a>
     <a href='{{ route('mesas.index') }}'>Mesas</a>
+    <a href='{{ route('categorias.index') }}'>Categor&iacute;as</a>
+    <a href='{{ route('productos.index') }}'>Productos</a>
     <div class="animation start-home"></div>
 </nav>
 
