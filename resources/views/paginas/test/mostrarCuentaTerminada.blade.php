a2039c39606529614e78d3ba1d8f4d2aa81d6241
DGB012 <dagonbu02@gmail.com>
2023-01-25 11:44:55 +0100

    Creo rama para añadir pantalla al finalizar cuenta.

---
+++ resources/views/paginas/test/mostrarCuentaTerminada.blade.php
@@ -0,0 +1,21 @@
+<x-zz.base>
+
+    <x-slot:tituloHead>Cuenta {{$cuenta->id}}</x-slot:tituloHead>
+    <x-slot:tituloVisible>Cuenta {{$cuenta->id}}</x-slot:tituloVisible>
+
+    <br><br>
+    <p>{{$cuenta->id}}</p>
+    @foreach ($categorias as $categoria)
+        <button
+            onclick="cambiarVisibilidad('{{$categoria->nombre}}', '{{count($categorias)}}')">{{$categoria->nombre}}</button>
+    @endforeach
+    <br><br>
+    @foreach ($productos as $producto)
+        <button class='prueba' name={{$producto->categoria->nombre}}>
+            <a href='{{ route('lineaCuenta.crearLineaNueva',['cuenta_id'=>$cuenta->id,'producto_id' => $producto->id]) }}'>{{$producto->nombre}}</a>
+        </button>
+    @endforeach
+    <br><br>
+    <button><a href="{{route('cuentas.terminarCuenta',$cuenta->id)}}">Terminar cuenta</a></button>
+
+</x-zz.base>
