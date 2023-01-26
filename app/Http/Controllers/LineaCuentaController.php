34fa21f288d33aa2346b49c847cea75e0412328e
ravl17 <ravlgg511@gmail.com>
2023-01-26 14:04:26 +0100

    LineaCuenta cantidad puede ser más de 1.

--- app/Http/Controllers/LineaCuentaController.php
+++ app/Http/Controllers/LineaCuentaController.php
@@ -123,13 +123,9 @@ class LineaCuentaController extends Controller
             $lineaCuenta->save();
 
         } else {
-
-            $linea = DB::table('linea_cuentas')
-                ->where([['cuentas_id', '=', $cuenta_id], ['producto_id', '=', $producto_id]])
-                ->first();/*->update(['cantidad' => 2])*/;
-            return ($linea);
-
-            /*NO ESTA TERMINADO*/
+            $lineaCuenta = LineaCuenta::firstOrCreate(['cuentas_id' => $cuenta_id, 'producto_id' => $producto_id]);
+            $lineaCuenta->cantidad=$lineaCuenta->cantidad+1;
+            $lineaCuenta->save();
         }
 
         return redirect()->route('mesas.index');
