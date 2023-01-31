f976441f6089df268c35c78a4a83d60afff489b3
afro <afro0698@gmail.com>
2023-01-31 12:43:42 +0100

    Index de mesas no va muy bien

--- resources/views/paginas/mesas/index.blade.php
+++ resources/views/paginas/mesas/index.blade.php
@@ -1,56 +1,29 @@
+<!--suppress CssUnknownTarget -->
+
 <x-zz.base>
 
     <x-slot:tituloHead>Mesas</x-slot:tituloHead>
     <x-slot:tituloVisible>Listado de Mesas</x-slot:tituloVisible>
 
-    <table border="2px">
-
-        <tr>
-            <th>Mesas</th>
-            <th>Estado</th>
-            <th>Eliminar</th>
-        </tr>
-
-        @foreach($mesas as $mesa)
-            <tr>
-                <td>
-                    <a href='{{ route ('cuentas.crearModifCuenta', $mesa -> id) }}'>
-                        <img src="/mesa.png" width="80px" height="10%">{{ $mesa-> id }}
+    <div class='container-fluid a'>
+        <div class="row ">
+            @foreach($mesas as $mesa)
+                <div class="col-1 b">
+                    <a href='{{ route("cuentas.crearModifCuenta",$mesa -> id) }}'>
+                        <img src='{{($mesa->estado == "Vacia") ? "/mesa".$mesa -> id."Dispo.png" : "/mesa".$mesa -> id."ocup.png"}}' >
                     </a>
-                </td>
-                <td>
-                    <a href='{{ route ('cuentas.crearModifCuenta', $mesa -> id) }}'>
-                        <img src='{{($mesa->estado == "Vacia") ? "/verde.png" : "/rojo.png"}}' class="estadoMesa">
-                    </a>
-                </td>
-                <td>
-                    <form action='{{ route('mesas.destroy', $mesa) }}' method="post">
-                        @method('delete')
-                        <button type='submit'><img src="basura.png" width="50px"></button>
-                    </form>
-                </td>
-            </tr>
-        @endforeach
+                </div>
+            @endforeach
+                @foreach($mesas as $mesa)
+                    <div class="col-1 c">
+                        <a href='{{ route("cuentas.crearModifCuenta",$mesa -> id) }}'>
+                            <img src='{{($mesa->estado == "Vacia") ? "/mesa1Dispo.png" : "/mesa1ocup.png"}}' >
+                        </a>
+                    </div>
+                @endforeach
+        </div>
+    </div>
 
-        <head><title>Mapa de Mesas</title></head>
-        <body>
-
-        <map name="mapa1">
-
-            <area shape="poly" coords="95,107,425,257" href=''>
-
-        </map>
-        <img src="/MapaMesas.png" usemap="#mapa1">
-
-        </body>
-
-
-
-
-    </table><br><br>
-
-    </table>
-    <br><br>
 
     <button><a href='{{ route('mesas.create') }}'> Crear </a></button>
     <br><br>
